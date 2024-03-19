482c693687592c48bf3a97674ad5c123c5246c4e
HugoNCastro <hugonaaman@gmail.com>
2024-03-19 11:00:47 -0300

    Improve next.config to ignore linting during build

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['github.com']
-  }
-};
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 
-export default nextConfig;
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  images: {
+    domains: ['github.com'],
+  },
+}
+
+export default nextConfig
