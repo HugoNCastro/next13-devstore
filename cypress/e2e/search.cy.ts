2ddac9611894673aacbcd7a0f078c527d7575a7f
HugoNCastro <hugonaaman@gmail.com>
2024-03-19 10:18:07 -0300

    Insert baseURL to cypress.config.ts
    Improve add products to cart test
    
    Create search page test

---
+++ cypress/e2e/search.cy.ts
@@ -0,0 +1,20 @@
+describe('add product to cart', () => {
+  it('should be able to search a product and add it to the cart', () => {
+    cy.visit('/')
+    cy.get('input[name=q]').type('moletom').parent('form').submit()
+
+    cy.location('pathname').should('include', '/search')
+    cy.location('search').should('include', 'q=moletom')
+
+    cy.get('a[href^="/product"]').should('exist')
+  })
+
+  it('should not be able to visist search page without search query', () => {
+    cy.on('uncaught:exception', () => {
+      return false
+    })
+
+    cy.visit('/search')
+    cy.location('pathname').should('equal', '/')
+  })
+})
